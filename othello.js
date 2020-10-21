53d3f646f0a29cb903a09b95d27285524fad2abf
gabrielasilva6 <71786476+gabrielasilva6@users.noreply.github.com>
2020-10-21 18:33:07 +0100

    Add files via upload
    Dialog box for rules/rules button

--- othello.js
+++ othello.js
@@ -1,7 +1,33 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 function gameon() {
     "use strict";
     document.getElementById("Status").style.display = 'block';
     document.getElementById("Config").style.display = 'block';
+    document.getElementById("myBtn").style.display = 'block';
 
 
     /*const board = document.getElementsByClassName("board");*/
