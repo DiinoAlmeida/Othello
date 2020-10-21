837f72ea6aa7fb214ed5792ed008fe8b159e5173
DiinoAlmeida <61515055+DiinoAlmeida@users.noreply.github.com>
2020-10-21 22:51:29 +0100

    Change color and select the initial player

--- othello.js
+++ othello.js
@@ -23,22 +23,79 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+
+
+/*
+    PlayersPos [] -> indica o "dono" de cada quadrado. Vai ser usado para decidir se há jogadas possíveis ou não.
+    
+    2 -> Discos Pretos
+    1 -> Discos Brancos
+    
+    NPlayer1 e NPlayer2 Nº total de peças de cada jogador
+    
+    O player1 vai ser sempre o primeiro a jogar e é o das peças pretas
+*/
+
+var PlayersPos = new Array (64);
+var NPlayer1 = 2;
+var NPlayer2 = 2;
+var Turn = "Player2";
+
+for(let i = 0; i < 64; i++){
+    PlayersPos[i] = 0;    
+}
+
+//Posições iniciais
+PlayersPos[28] = 2;
+PlayersPos[35] = 2;
+PlayersPos[27] = 1;
+PlayersPos[36] = 1;
+
+console.log(PlayersPos);
+
 function gameon() {
     "use strict";
     document.getElementById("Status").style.display = 'block';
     document.getElementById("Config").style.display = 'block';
-    document.getElementById("myBtn").style.display = 'block';
-
+        document.getElementById("myBtn").style.display = 'block';
 
     /*const board = document.getElementsByClassName("board");*/
     const board = document.createElement("div");
     board.className = "board";
 
-
-    for (var i = 0; i < 64; i++) {
+    for (let i = 0; i < 64; i++) {
         const square = document.createElement("div");
         square.className = "square";
+        square.id = i;
         board.appendChild(square);
     }
+    
     document.body.appendChild(board);
+    
+    const cells = document.querySelectorAll(".square");
+    
+    document.getElementById("28").style.backgroundColor = "black"; 
+    document.getElementById("35").style.backgroundColor = "black"; 
+    document.getElementById("27").style.backgroundColor = "white"; 
+    document.getElementById("36").style.backgroundColor = "white";
+    
+    for (let i = 0; i < cells.length; i++) {
+        if(i !=  28 && i != 35 && i != 27 && i != 36){
+            cells[i].addEventListener('click', turnClick,);
+        }
+	}
+      
 }
+
+
+function turnClick(square) {
+    if(Turn == "Player1"){
+        document.getElementById(square.target.id).style.backgroundColor = "black";
+    }else{
+        document.getElementById(square.target.id).style.backgroundColor = "white";
+    }
+}
+
+
