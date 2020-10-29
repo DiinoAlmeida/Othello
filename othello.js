dc4734214d8e1a83eaf43052a8b563721f772fa4
gabrielasilva6 <71786476+gabrielasilva6@users.noreply.github.com>
2020-10-29 15:30:27 +0000

    Add files via upload

--- othello.js
+++ othello.js
@@ -1,38 +1,20 @@
-// Get the modal
+// Modal for rules button
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function () {
     modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
-
-
-
-
-
 //----------------------------------------------------------------
-
-
-
 
 
 class Player {
@@ -88,6 +70,10 @@ function gameon() {
     document.getElementById("Status").style.display = 'block';
     document.getElementById("Config").style.display = 'block';
     document.getElementById("myBtn").style.display = 'block';
+    document.getElementById("DiscosB").style.display = 'block';
+    document.getElementById("DiscosP").style.display = 'block';
+    //document.getElementById("DiscosB").innerHTML += 2;
+    //document.getElementById("DiscosP").innerHTML += 2;
 
     const board = document.createElement("div");
     board.className = "board";
@@ -206,6 +192,12 @@ function analiseNeighbors(i, j) {
                         document.getElementById(z).style.backgroundColor = Turn.cor;
                         PosColor[r][c] = Turn.cor;
                         Turn.Npecas++;
+                        if(Turn.cor == "white") {
+                          document.getElementById("DiscosB").innerHTML = "Discos brancos:" + Turn.Npecas;
+                        } else {
+                          document.getElementById("DiscosP").innerHTML = "Discos pretos:" + Turn.Npecas;
+                        }
+
                         count--;
                     }
                     status = 1;
