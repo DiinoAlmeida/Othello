d7d794cdc3b2bbba41746d078100c5b2269d734e
DiinoAlmeida <61515055+DiinoAlmeida@users.noreply.github.com>
2020-10-31 14:11:31 +0000

    Add files via upload

--- othello.js
+++ othello.js
@@ -25,9 +25,12 @@ window.onclick = function (event) {
 }
 
 
+/*
+    Ver porque quando a ia não pode jogar nem o jogador pode (por esta ordem) a IA joga
+*/
+
 
 //----------------------------------------------------------------
-
 class Player {
 
     constructor(id, Npecas) {
@@ -35,20 +38,13 @@ class Player {
         this.Npecas = Npecas;
         this.vitorias = 0;
         this.cor = null;
+        this.className = null;
     }
 
 }
 
 //Create a 2d array and set every position equal to null
 var PosCor = new Array(8);
-
-for (let i = 0; i < 8; i++) {
-    PosCor[i] = new Array(8);
-    for (let j = 0; j < 8; j++) {
-        PosCor[i][j] = "null";
-    }
-}
-
 var Player1 = new Player(1, 2);
 var Player2 = new Player(2, 2);
 var cells;
@@ -62,25 +58,23 @@ function submit() {
 
     if (radioCor[0].checked) {
         Player1.cor = "black";
+        Player1.className = "squareB";
         Player2.cor = "white";
-        document.getElementById("PlayerTurn").innerHTML = "Black turn";
+        Player2.className = "squareW";
         Turn = Player1;
     } else {
         Player1.cor = "white";
+        Player1.className = "squareW";
         Player2.cor = "black";
+        Player2.className = "squareB";
         Turn = Player2;
         bestMove();
-        document.getElementById("PlayerTurn").innerHTML = "White turn";
     }
+    document.getElementById("PlayerTurn").innerHTML = "Jogador";
 }
-
-//Cores iniciais
-PosCor[3][3] = PosCor[4][4] = "white";
-PosCor[3][4] = PosCor[4][3] = "black";
 
 function gameon() {
     "use strict"; //????
-
     document.getElementById("Access").style.display = "none";
     document.getElementById("Status").style.display = 'block';
     document.getElementById("Config").style.display = 'block';
@@ -89,6 +83,7 @@ function gameon() {
     document.getElementById("DiscosP").style.display = 'block';
     document.getElementById("PlayerTurn").style.display = 'block';
 
+    ArrayInit();
 
     const board = document.createElement("div");
     board.className = "board";
@@ -107,30 +102,52 @@ function gameon() {
 
     cells = document.querySelectorAll(".square");
 
-    document.getElementById("28").style.backgroundColor = "black";
-    document.getElementById("35").style.backgroundColor = "black";
-    document.getElementById("27").style.backgroundColor = "white";
-    document.getElementById("36").style.backgroundColor = "white";
+    document.getElementById("28").className = "squareB";
+    document.getElementById("35").className = "squareB";
+    document.getElementById("27").className = "squareW";
+    document.getElementById("36").className = "squareW";
 
     document.getElementById("DiscosB").innerHTML = "Discos brancos: 2";
     document.getElementById("DiscosP").innerHTML = "Discos pretos: 2";
-
-
 }
 
+function PlayAgain() {
+
+    document.getElementById("Again").disabled = true;
+    document.getElementById("Passar").style.display = 'block';
+    document.getElementById("PlayerTurn").style.display = 'block';
+    document.getElementById("Vencedor").style.display = 'none';
+    document.getElementById("Vencedor").innerHTML = "";
+
+    ArrayInit();
+
+    Player1.Npecas = Player2.Npecas = 2;
+
+    for (let i = 0; i < 64; i++) {
+        document.getElementById(i).className = "square";
+    }
+
+    document.getElementById("28").className = "squareB";
+    document.getElementById("35").className = "squareB";
+    document.getElementById("27").className = "squareW";
+    document.getElementById("36").className = "squareW";
+
+    submit();
+}
 
 function bestMove() {
 
     for (let i = 0; i <= 7; i++) {
         for (let j = 0; j <= 7; j++) {
             if (PosCor[i][j] != Turn.cor && (analiseNeighbors(i, j, 1) == 1)) {
-                document.getElementById(returnId(i, j)).disabled = "true";
+                let z = returnId(i, j);
+                document.getElementById(z).disabled = "true";
                 swapTurn(); // Jogador
 
-                if (EndGame() == true) { //Verficar se jogador pode jogar
+                if (CheckEndGame() == true) { //Verficar se jogador pode jogar
                     swapTurn();
-                    if (EndGame() == true) { // Verificar se IA pode jogar 
-                        window.alert("O JOGO ACABOU!!");
+                    if (CheckEndGame() == true) { // Verificar se IA pode jogar 
+                        EndGame();
                     } else {
                         bestMove();
                     }
@@ -146,24 +163,31 @@ function turnClick(indice) {
     if (playMove(indice) == 1) {
         document.getElementById(indice).disabled = "true";
         swapTurn(); //IA
-        if (EndGame() == false) { // Se a IA puder jogar joga, caso contrario passa a jogada
+        if (CheckEndGame() == false) { // Se a IA puder jogar joga, caso contrario passa a jogada
             setTimeout(bestMove, 2000);
         } else {
             swapTurn(); //Jog
-            if(EndGame() == true){
-                document.getElementById("Passar").style.display='none';
-                document.getElementById("PlayerTurn").style.display='none';
-                document.getElementById("Vencedor").style.display='block';
-                if(Player1.Npecas > Player2.Npecas){
-                    document.getElementById("Vencedor").innerHTML = "Parabéns ganhou!!";
-                }else{
-                     document.getElementById("Vencedor").innerHTML = "Fica para a próxima, jogar outra vez?";
-                }                
-            }else{
-                 console.log("A ai passou a jogada");
-            }
+            if (CheckEndGame() == true) EndGame();
+            document.getElementById("Again").disabled = false;
         }
     }
+}
+
+
+
+function EndGame() {
+
+    document.getElementById("Passar").style.display = 'none';
+    document.getElementById("PlayerTurn").style.display = 'none';
+    document.getElementById("Vencedor").style.display = 'block';
+    if (Player1.Npecas > Player2.Npecas) {
+        document.getElementById("Vencedor").innerHTML = "Parabéns ganhou!!";
+        Player1.vitorias++;
+    } else {
+        Player2.vitorias++;
+        document.getElementById("Vencedor").innerHTML = "Fica para a próxima, jogar outra vez?";
+    }
+    
 }
 
 //Esta função vai decidir se onde clicamos é possivel jogar ou nao consoante as regras
@@ -220,7 +244,7 @@ function analiseNeighbors(i, j, option) {
                             r = r - dr;
                             c = c - dc;
                             z = returnId(r, c);
-                            document.getElementById(z).style.backgroundColor = Turn.cor;
+                            document.getElementById(z).className = Turn.className;
                             PosCor[r][c] = Turn.cor;
                             count--;
                         }
@@ -236,7 +260,7 @@ function analiseNeighbors(i, j, option) {
 
 
 function returnId(i, j) {
-    
+
     let count = 0;
     while (i > 0) {
         count += 8;
@@ -250,17 +274,17 @@ function returnId(i, j) {
 function swapTurn() {
 
     if (Turn == Player1) {
-        document.getElementById("PlayerTurn").innerHTML = "White turn";
+        document.getElementById("PlayerTurn").innerHTML = "Computador";
         Turn = Player2;
     } else {
-        document.getElementById("PlayerTurn").innerHTML = "Black turn";
+        document.getElementById("PlayerTurn").innerHTML = "Jogador";
         Turn = Player1;
     }
- 
+
     document.getElementById("Passar").style.display = 'none';
 }
 
-function EndGame() {
+function CheckEndGame() {
 
     for (let i = 0; i <= 7; i++) {
         for (let j = 0; j <= 7; j++) {
@@ -271,8 +295,10 @@ function EndGame() {
     return true;
 }
 
+
+
 function skipTurn() {
-    if (EndGame() == false) {
+    if (CheckEndGame() == false) {
         document.getElementById("Passar").style.display = 'block';
     } else {
         swapTurn();
@@ -280,28 +306,42 @@ function skipTurn() {
     }
 }
 
-function contarPecas(){
-    
+function contarPecas() {
+
     let count1 = 0;
     let count2 = 0;
-    
-    for(let i = 0; i <= 7; i++){
-        for(let j = 0; j <= 7; j++){
-            if(PosCor[i][j] == Player1.cor){
+
+    for (let i = 0; i <= 7; i++) {
+        for (let j = 0; j <= 7; j++) {
+            if (PosCor[i][j] == Player1.cor) {
                 count1++;
-            }else if(PosCor[i][j] == Player2.cor){
+            } else if (PosCor[i][j] == Player2.cor) {
                 count2++;
             }
         }
     }
-    
+
     Player1.Npecas = count1;
     Player2.Npecas = count2;
-    
-    document.getElementById("DiscosB").innerHTML = "Discos brancos: " + count2;
-    document.getElementById("DiscosP").innerHTML = "Discos pretos: " + count1;
+
+    if (Player1.cor == "black") {
+        document.getElementById("DiscosB").innerHTML = "Discos brancos: " + count2;
+        document.getElementById("DiscosP").innerHTML = "Discos pretos: " + count1;
+    } else {
+        document.getElementById("DiscosB").innerHTML = "Discos brancos: " + count1;
+        document.getElementById("DiscosP").innerHTML = "Discos pretos: " + count2;
+    }
 }
 
 
+function ArrayInit() {
+    for (let i = 0; i < 8; i++) {
+        PosCor[i] = new Array(8);
+        for (let j = 0; j < 8; j++) {
+            PosCor[i][j] = "null";
+        }
+    }
 
-
+    PosCor[3][3] = PosCor[4][4] = "white";
+    PosCor[3][4] = PosCor[4][3] = "black";
+}
