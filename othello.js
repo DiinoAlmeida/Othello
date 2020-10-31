d51fe79f49e72d1a2415527148e5fbb3fe7af3fb
DiinoAlmeida <61515055+DiinoAlmeida@users.noreply.github.com>
2020-10-31 17:38:23 +0000

    Add files via upload

--- othello.js
+++ othello.js
@@ -29,7 +29,6 @@ window.onclick = function (event) {
     Ver porque quando a ia não pode jogar nem o jogador pode (por esta ordem) a IA joga
 */
 
-
 //----------------------------------------------------------------
 class Player {
 
@@ -43,38 +42,18 @@ class Player {
 
 }
 
-//Create a 2d array and set every position equal to null
+//Inicializar variaveis globais
 var PosCor = new Array(8);
 var Player1 = new Player(1, 2);
 var Player2 = new Player(2, 2);
-var cells;
 var Turn;
 
-//Mudar as opções do jogo
-function submit() {
-    radioCor = document.getElementsByName("Cor");
-    radioAdeversario = document.getElementsByName("Adversario");
-    radioDificuldade = document.getElementsByName("Dificuldade");
-
-    if (radioCor[0].checked) {
-        Player1.cor = "black";
-        Player1.className = "squareB";
-        Player2.cor = "white";
-        Player2.className = "squareW";
-        Turn = Player1;
-    } else {
-        Player1.cor = "white";
-        Player1.className = "squareW";
-        Player2.cor = "black";
-        Player2.className = "squareB";
-        Turn = Player2;
-        bestMove();
-    }
-    document.getElementById("PlayerTurn").innerHTML = "Jogador";
-}
-
+/*
+    Depois de pressionar botão login colocamos todos os elementos necessários
+    na página e criamos o tabuleiro.
+*/
 function gameon() {
-    "use strict"; //????
+    "use strict";
     document.getElementById("Access").style.display = "none";
     document.getElementById("Status").style.display = 'block';
     document.getElementById("Config").style.display = 'block';
@@ -103,7 +82,7 @@ function gameon() {
 
     document.body.appendChild(board);
 
-    cells = document.querySelectorAll(".square");
+    //Cells = document.querySelectorAll(".square");
 
     document.getElementById("28").className = "squareB";
     document.getElementById("35").className = "squareB";
@@ -114,10 +93,36 @@ function gameon() {
     document.getElementById("DiscosP").innerHTML = "Discos pretos: 2";
 }
 
+//Processar as opções de jogo escolhidas
+function submit() {
+    radioCor = document.getElementsByName("Cor");
+    radioAdeversario = document.getElementsByName("Adversario");
+    radioDificuldade = document.getElementsByName("Dificuldade");
+
+    if (radioCor[0].checked) {
+        Player1.cor = "black";
+        Player1.className = "squareB";
+        Player2.cor = "white";
+        Player2.className = "squareW";
+        Turn = Player1;
+    } else {
+        Player1.cor = "white";
+        Player1.className = "squareW";
+        Player2.cor = "black";
+        Player2.className = "squareB";
+        Turn = Player2;
+        bestMove();
+    }
+
+    document.getElementById("PlayerTurn").innerHTML = "Jogador";
+    document.getElementById("Play").disabled = true;
+}
+
+//Botão de voltar a jogar, fazemos "reset" a tudo
 function PlayAgain() {
 
     document.getElementById("Again").disabled = true;
-    document.getElementById("Passar").style.display = 'block';
+    document.getElementById("Passar").style.display = 'none';
     document.getElementById("PlayerTurn").style.display = 'block';
     document.getElementById("Vencedor").style.display = 'none';
     document.getElementById("Vencedor").innerHTML = "";
@@ -138,6 +143,7 @@ function PlayAgain() {
     submit();
 }
 
+//Vez do computador a jogar
 function bestMove() {
 
     for (let i = 0; i <= 7; i++) {
@@ -149,7 +155,7 @@ function bestMove() {
 
                 if (CheckEndGame() == true) { //Verficar se jogador pode jogar
                     swapTurn();
-                    if (CheckEndGame() == true) { // Verificar se IA pode jogar
+                    if (CheckEndGame() == true) { // Verificar se IA pode jogar 
                         EndGame();
                     } else {
                         bestMove();
@@ -161,6 +167,7 @@ function bestMove() {
     }
 }
 
+//Depois de clicarmos no tabuleiro
 function turnClick(indice) {
 
     if (playMove(indice) == 1) {
@@ -171,28 +178,8 @@ function turnClick(indice) {
         } else {
             swapTurn(); //Jog
             if (CheckEndGame() == true) EndGame();
-            document.getElementById("Again").disabled = false;
         }
     }
-}
-
-
-
-function EndGame() {
-
-    document.getElementById("Passar").style.display = 'none';
-    document.getElementById("PlayerTurn").style.display = 'none';
-    document.getElementById("Vencedor").style.display = 'block';
-    if (Player1.Npecas > Player2.Npecas) {
-        document.getElementById("Vencedor").innerHTML = "Parabéns ganhou!!";
-        Player1.vitorias++;
-    } else {
-        Player2.vitorias++;
-        document.getElementById("Vencedor").innerHTML = "Fica para a próxima, jogar outra vez?";
-    }
-    document.getElementById("JogadorWin").innerHTML = "Jogador: " + Player1.vitorias;
-    document.getElementById("ComputerWin").innerHTML = "Computador: " +Player2.vitorias;
-
 }
 
 //Esta função vai decidir se onde clicamos é possivel jogar ou nao consoante as regras
@@ -201,7 +188,6 @@ function playMove(indice) {
 
     let status = -1;
 
-    //find line and collumn of indice
     let count = 0;
     while (indice > 7) {
         indice -= 8;
@@ -263,7 +249,7 @@ function analiseNeighbors(i, j, option) {
     return status;
 }
 
-
+//Vai returna o id do quadrado selecionado para usar no PosCor
 function returnId(i, j) {
 
     let count = 0;
@@ -276,6 +262,7 @@ function returnId(i, j) {
     return count;
 }
 
+//Faz a troca de turnos
 function swapTurn() {
 
     if (Turn == Player1) {
@@ -289,6 +276,7 @@ function swapTurn() {
     document.getElementById("Passar").style.display = 'none';
 }
 
+
 function CheckEndGame() {
 
     for (let i = 0; i <= 7; i++) {
@@ -300,8 +288,28 @@ function CheckEndGame() {
     return true;
 }
 
+//Termina o jogo
+function EndGame() {
 
+    document.getElementById("Passar").style.display = 'none';
+    document.getElementById("PlayerTurn").style.display = 'none';
+    document.getElementById("Vencedor").style.display = 'block';
+    document.getElementById("Again").disabled = false;
+    
+    if (Player1.Npecas > Player2.Npecas) {
+        document.getElementById("Vencedor").innerHTML = "Parabéns ganhou!!";
+        Player1.vitorias++;
+    } else {
+        Player2.vitorias++;
+        document.getElementById("Vencedor").innerHTML = "Fica para a próxima, jogar outra vez?";
+    }
 
+    document.getElementById("JogadorWin").innerHTML = "Jogador: " + Player1.vitorias;
+    document.getElementById("ComputerWin").innerHTML = "Computador: " + Player2.vitorias;
+
+}
+
+//Passar a jogada
 function skipTurn() {
     if (CheckEndGame() == false) {
         document.getElementById("Passar").style.display = 'block';
@@ -311,6 +319,7 @@ function skipTurn() {
     }
 }
 
+//Conta as peças para a atualizar o status
 function contarPecas() {
 
     let count1 = 0;
@@ -338,7 +347,7 @@ function contarPecas() {
     }
 }
 
-
+//Inicia o array a null e coloca as cores de inicio de jogo
 function ArrayInit() {
     for (let i = 0; i < 8; i++) {
         PosCor[i] = new Array(8);
